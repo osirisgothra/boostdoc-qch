d5c7e48db243e3e4d9535dc5fd65009661c411b1
Gabriel T. Sharp <osirisgothra@hotmail.com>
2014-06-17 09:31:10 -0400

    experimental-commit

--- generatelists.sh
+++ generatelists.sh
@@ -11,13 +11,57 @@
 # warning: the original lists will be deleted without mercy
 #
 
-echo "generating file lists, this make take a second..."
-echo "removing old (if existing)"
-rm files-unformatted.list -f
-rm files.list -f
+[[ $1 ]] || set -- --noopt
+
+# required bash shell options
+
+shopt -s nullglob
+shopt -s globstar
+shopt -s ext
+
+case $1 in
+ --backup)
+ 	[[ $2 ]] || set -- $1 "/tmp"
+ 	echo "moving old files to $2..."
+	mv files-unformatted.list "$2" -v
+	mv files.list "$2" -v
+	;;
+ --noopt)
+	echo "generating file lists, this make take a second..."
+	echo "removing old (if existing)"
+	rm files-unformatted.list -f
+	rm files.list -f
+	;;
+esac
+
 echo "generating files.list (used for insertion into qhp files)..."
 find -regextype posix-egrep -iregex '.*\.(png|html|css|htm)' | sed --regexp-extended 's/^(\.\/)(.*)$/<file>\2<\/file>/g' > files.list
 echo "generating files-unformatted.list (used for keyword collection and link-rewriting operations)..."
-find -regextype posix-egrep -iregex '.*\.(png|html|css|htm)' | sed --regexp-extended 's/^(\.\/)(.*)$/\2/g' > files-unformatted.list
+find -regextype posix-egrep -iregex '.*' | sed --regexp-extended 's/^(\.\/)(.*)$/\2/g' > files-unformatted.list.pre
+echo "polishing files-unformatted.list..."
+cat files-unformatted.list.pre | grep ".*\.git.*" --invert-match > files-unformatted.list.pre2
+# polish by entry start
+mapfile ITEMS < files-unformatted.list.pre
+touch files-unformatted.list
+for item in "${ITEMS[@]}"; do
+	unset ADDIT   	
+	if [[ -d "$item" ]]; then
+		if (ls $item/*.html | grep -P '\b\.html\b' -q); then
+	 		ADDIT=TRUE
+		fi	 
+	else
+		if [[ ${item##*.} == html ]]; then
+			ADDIT=TRUE
+		fi
+	if [[ $ADDIT == TRUE ]]; then
+    echo "$item" >> files-unformatted.list
+	fi
+done
+
+# polish by entry end
+
+echo "cleaning up intermediate files..."
+rm files-unformatted.list.pre  -f 
+rm files-unformatted.list.pre2 -f
 echo "done."
 
--- generatesections.sh
+++ generatesections.sh
@@ -25,7 +25,29 @@ function get_html_title()
 # get_file_section_entry [filename]
 function get_file_section_entry()
 {
-	
+	SECTIONTITLE=$(get_html_title "$1")
+	if [[ -z $SECTIONTITLE ]]; then
+		return 1
+	else
+		unset INDENTS
+		declare -i MAXINDENTS=10
+		while (true); do		
+			DIRNAME=$(dirname $DIRNAME)
+			if [[ $DIRNAME == $PWD ]]; then
+				break
+			else
+				INDENTS+='\t'
+				((MAXINDENTS--))
+			fi
+			if ((MAXINDENTS==0)); then
+				break
+			fi
+		done
+		
+
+		echo "$INDENTS<section title=\"$SECTIONTITLE\" ref=\"$1\">"
+	  return 0
+	fi
 }
 # returns [true] = 
 #   <section title="Title Text" ref="file.name">
@@ -40,12 +62,13 @@ function get_section_endtag()
 
 echo "generating sections, this make take a second..."
 echo "removing old (if existing)"
-rm sections.list -f
+#rm sections.list -f
 echo "generating sections.list (used for insertion into qhp files)..."
 # TODO: do the generation
 # for now i only create the file itself so it is not missed in the generation of the project
 # ------------------------------------------------------------------------------------------
-touch sections.list
+mapfile ITEMS < files-unformatted.list
+
 # ------------------------------------------------------------------------------------------
 echo "done."
 
